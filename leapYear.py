19facdb81e8d89acbfbacff730300ec0d5d80c58
Angel <94824625+ataraxygale@users.noreply.github.com>
2025-03-26 15:52:47 -0600

    Update leapYear.py

--- leapYear.py
+++ leapYear.py
@@ -1,46 +1,25 @@
-#input year to determine if it's a leap year or not
-try:
-    year = int(input("Input a Year: "))
-except ValueError:
-    print("Invalid input: Please enter a valid number integer year")
 
-#now runs only after verifying input
-else:
-    if year % 4 != 0:
-       print("This is a common year")
-    elif year % 100 != 0:
-        print("This is a leap year! *frog noises*")
-    elif year % 400 != 0:
-        print("This is a common year")
+while True:
+    #input year to determine if it's a leap year or not
+    try:
+        year = int(input("Input a Year: "))
+    except ValueError:
+        print("Invalid input: Please enter a valid number integer year\n")
+
+    #now runs only after verifying input
     else:
-        print("This is a leap year! *frog noises*")
+        if year % 4 != 0:
+           print(f"{year} is a common year\n")
+        elif year % 100 != 0:
+            print(f"{year} is a leap year! *frog noises*\n")
+        elif year % 400 != 0:
+            print(f"{year} is a common year\n")
+        else:
+            print(f"{year} is a leap year! *frog noises*\n")
 
+    zero=input("Enter 0 to exit the program or any other key to continue\n")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if zero == '0':
+        exit()
 
 
